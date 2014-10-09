b959e798585a88cbb491c11fa9731d401b8b1398
tonyhayes <tony.magellan@gmail.com>
2014-10-09 16:19:23 -0500

    add application package for distribution

---
+++ Executive Dashboard/js/sencha/ExecutiveDashboard/app/Application.js
@@ -0,0 +1,33 @@
+/**
+ * The main application class. An instance of this class is created by app.js when it calls
+ * Ext.application(). This is the ideal place to handle application launch and initialization
+ * details.
+ */
+Ext.define('ExecDashboard.Application', {
+    extend: 'Ext.app.Application',
+
+    requires: [
+        'ExecDashboard.util.Share'
+    ],
+
+    name: 'ExecDashboard',
+
+    // The tab we want to activate if there is no "#tag" in the URL.
+    defaultToken: '!kpi/clicks',
+
+    views: [
+        'ExecDashboard.view.main.Main'
+    ],
+
+    launch: function () {
+        // Let's add a CSS class to body if flex box wrap is not implemented or broken
+        // http://flexboxlayouts.com/flexboxlayout_tricks.html
+        if (Ext.browser.is.Gecko && Ext.browser.version.major < 28) {
+            Ext.getBody().addCls('x-flex-wrap-broken');
+        }
+
+        //register the app as a Win8 Share Source
+        var dtm = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+        dtm.addEventListener("datarequested", ExecDashboard.util.Share.shareCanvasImage);
+    }
+});
---
+++ Executive Dashboard/js/sencha/packages/ext-win8/overrides/Win8.js
@@ -0,0 +1,43 @@
+﻿//@ override Ext.env.Ready
+(function (E) {
+    var v = E.getVersion();
+
+    if (v.major < 5 || (v.major === 5 && v.minor === 0 && v.patch === 0)) {
+        //<debug>
+        E.Error.raise('The "ext-win8" package must be used with Ext JS 5.0.1 or higher.');
+        //</debug>
+        return;
+    }
+
+    else if (v.major === 5 && v.minor === 0 && v.patch < 2) {
+        /*
+         * EXTJS-14614 is fixed in Ext JS > 5.0.1
+         */
+        (function (Ready) {
+            var invokeAll = Ready.invokeAll;
+            Ready.invokeAll = function () {
+                return MSApp.execUnsafeLocalFunction(function () {
+                    return invokeAll.call(Ready);
+                });
+            };
+        })(E.env.Ready);
+
+        (function (EXT) {
+            var MScallback = EXT.callback;
+
+            EXT.callback = function (callback, scope, args, delay, caller, defaultScope) {
+                return MSApp.execUnsafeLocalFunction(function () {
+                    return MScallback.call(EXT, callback, scope, args, delay, caller, defaultScope);
+                });
+            };
+        })(E);
+    }
+})(Ext);
+
+Ext.elevateFunction = function (fn, scope, args) {
+    //to satisfy the Windows 8 CSP:
+    // http://msdn.microsoft.com/en-us/library/windows/apps/hh767331....
+    return MSApp.execUnsafeLocalFunction(function () {
+        return fn.apply(scope, args);
+    });
+};
